8969debdd528460e7f356a10519e8a38aa8a59e3
mguzmanp <matias.guzmanparra@gmail.com>
2024-03-19 11:33:43 -0300

    MG: add support for effector offset

--- mlf_api/inverse_kinematics.py
+++ mlf_api/inverse_kinematics.py
@@ -1,20 +1,19 @@
 import numpy as np
 
-def inverse_kinematics(X=150,Y=0,Z=60):
+def inverse_kinematics(X=150,Y=0,Z=60, eff_off_x=65, eff_off_z=0):
 
-    offset_z = 100
-    offset_x = 56
     link_lengths = [55, 39, 135, 147, 66.3]
-    zb = Z - offset_z
+    offset_z = 100
+    zb = Z - offset_z - eff_off_z
     l1 = link_lengths[2]
     l2 = link_lengths[3]
     
     q0 = np.arctan2(Y,X)
 
     if np.abs(Y)<1e-2:
-        xo = X-offset_x
+        xo = X-eff_off_x
     else:
-        xo = np.sqrt((X-offset_x*np.cos(q0))**2 + (Y-offset_x*np.sin(q0))**2)
+        xo = np.sqrt((X-eff_off_x*np.cos(q0))**2 + (Y-eff_off_x*np.sin(q0))**2)
 
     q1 = np.pi - np.arctan2(zb,xo) - np.arccos((xo**2 + zb**2 + l1**2-l2**2)/(2*l1*np.sqrt(xo**2 + zb**2)))
     q2 = np.pi/2 - q1 + np.arccos((l1**2+l2**2-xo**2-zb**2)/(2*l1*l2))
