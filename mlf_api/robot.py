6135612af3a1611f7e08523aa21b51af2525751b
Renaranananna <Renato.andaur@gmail.com>
2024-05-24 10:55:54 -0400

    feat(client): Add request session

--- mlf_api/robot.py
+++ mlf_api/robot.py
@@ -17,6 +17,7 @@ class RobotClient:
         self.port = port
         self.base_url = f"http://{address}:{port}"
         self.connected = False
+        self.session = requests.Session()
         self.webRTCUser = WebRTCController(self.address)
 
     def connect(self):
@@ -25,7 +26,7 @@ class RobotClient:
             return
 
         url = f"{self.base_url}/connect"
-        response = requests.get(url)
+        response = self.session.get(url)
         if response.status_code == 200:
             self.connected = True
             print(response.text)
@@ -35,20 +36,20 @@ class RobotClient:
         q0, q1, q2 = inverse_kinematics(x, y, z, eff_off_x, eff_off_z)
         params = {"q0": q0, "q1": q1, "q2": q2, "q3": q3}
         url = f"{self.base_url}/set_joints"
-        response = requests.get(url, params=params)
+        response = self.session.get(url, params=params)
         print(response.text)
         
 
     def set_joints(self, q0=0, q1=0, q2=90, q3=120):
         params = {"q0": q0, "q1": q1, "q2": q2, "q3": q3}
         url = f"{self.base_url}/set_joints"
-        response = requests.get(url, params=params)
+        response = self.session.get(url, params=params)
         print(response.text)
 
     def set_relay_status(self, state=1, relay=1):
         params = {"state": state, "n_relay": relay}
         url = f"{self.base_url}/set_relay_status"
-        response = requests.get(url, params=params)
+        response = self.session.get(url, params=params)
         print(response.text)
     
     def connectWebRTC(self):
@@ -57,18 +58,18 @@ class RobotClient:
     def set_extra_servo(self, q=0):
         params = {"q": q}
         url = f"{self.base_url}/set_extra_servo"
-        response = requests.get(url, params=params)
+        response = self.session.get(url, params=params)
         print(response.text)
 
     def set_gripper_servo(self, q=120):
         params = {"q": q}
         url = f"{self.base_url}/set_gripper_servo"
-        response = requests.get(url, params=params)
+        response = self.session.get(url, params=params)
         print(response.text)
     
     def get_weight(self):
         url = f"{self.base_url}/get_weight"
-        response = requests.get(url)
+        response = self.session.get(url)
         json_data = json.loads(response.text)
         return json_data['weight']
 
