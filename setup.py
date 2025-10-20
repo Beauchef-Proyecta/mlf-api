5181faba6597aad4b05cb7ff146b6fd0b9b7dc23
Matías Guzmán <mguzmanp>
2025-10-20 11:56:45 -0300

    MG: unlock aiortc version hotfix

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(),
     install_requires = [
       'aiohttp==3.8.4',
-      'aiortc==1.5.0',
+      'aiortc',
       'numpy==1.24.3',
       'opencv-python==4.7.0.72',
       'requests==2.31.0'
